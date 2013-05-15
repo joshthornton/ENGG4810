4e3a33cae8c67620f0bbf0615d205b2f427c4350
Joshua Thornton <JoshuaMThornton@gmail.com>
2013-05-15 10:45:45 +1000

    buffer code now has fixed size circular buffer for echo and delay

--- ENGG4810_firmware/buffer.h
+++ ENGG4810_firmware/buffer.h
@@ -1,20 +1,47 @@
 #ifndef _ENGG4810_BUFFER_H_
 #define _ENGG4810_BUFFER_H_
 
-#define BUFFER_SIZE (1024)
+#define BUFFER_SIZE (4096)
 
 typedef struct buffer {
 	signed short b[BUFFER_SIZE];
 	unsigned long start;
 	unsigned long length;
+	unsigned long size;
 } buffer;
 
-void buffer_init( buffer* b );
-unsigned long buffer_read( buffer* b, signed short dst[], unsigned long maxlen );
-unsigned long buffer_write( buffer* b, signed short src[], unsigned long maxlen );
-inline unsigned long buffer_free_space( buffer* b )
+// Set the circular buffer size.
+// Effectively delay/echo time
+inline void buffer_set_size( buffer * b, unsigned long size )
 {
-	return BUFFER_SIZE - b->length;
+	b->size = ( size < BUFFER_SIZE ) ? size : BUFFER_SIZE;
+}
+
+// Always succeeds
+// will overwrite value b->size values ago
+inline void buffer_push( buffer *b, signed short val )
+{
+	b->b[b->start] = val;
+	b->start = ( b->start + 1 ) % b->size;
+}
+
+// Always succeeds
+// will return the oldest value within b->size
+inline signed short buffer_pop( buffer *b )
+{
+	return b->b[ ( b->start + 1 ) % b->size ];
+}
+
+inline void buffer_init( buffer *b )
+{
+	unsigned long i;
+	b->start = 0;
+	b->length = 0;
+	b->size = 0;
+
+	// Initialize to zero
+	for ( i = 0; i < BUFFER_SIZE; ++i )
+		b->b[i] = 0;
 }
 
 #endif
